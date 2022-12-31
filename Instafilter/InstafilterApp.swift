b5f3beec633cf2682261afdb70fdeaafde86fc47
P Deepanshu <97436890+deep60@users.noreply.github.com>
2022-12-31 08:27:24 +0530

    Add files via upload

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by P Deepanshu on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
